ded3927acc50286efc09f97c98b13381a781ec71
Micheal Limones <micheal@zestylimones.com>
2023-03-16 16:45:25 -0500

    added in authentication.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,60 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { oktaCongif } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { Security, LoginCallback } from '@okta/okta-react';
+import LoginWidget from './Auth/LoginWidget';
+
+const oktaAuth = new OktaAuth(oktaCongif);
 
 export const App = () => {
+  const customAuthHandler = () => {
+    history.push('/login');
+  };
+
+  const history = useHistory();
+  const restoreOriginalUri = async (_oktaAuth: any, orginialUri: any) => {
+    history.replace(toRelativeUrl(orginialUri || '/', window.location.origin));
+  };
+
   return (
     <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <div className="flex-grow-1">
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/home" />
-          </Route>
-          <Route path="/home">
-            <HomePage />
-          </Route>
-          <Route path="/search">
-            <SearchBooksPage />
-          </Route>
-          <Route path="/checkout/:bookId">
-            <BookCheckoutPage />
-          </Route>
-        </Switch>
-      </div>
+      <Security
+        oktaAuth={oktaAuth}
+        restoreOriginalUri={restoreOriginalUri}
+        onAuthRequired={customAuthHandler}
+      >
+        <Navbar />
+        <div className="flex-grow-1">
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/home" />
+            </Route>
+            <Route path="/home">
+              <HomePage />
+            </Route>
+            <Route path="/search">
+              <SearchBooksPage />
+            </Route>
+            <Route path="/checkout/:bookId">
+              <BookCheckoutPage />
+            </Route>
+            <Route
+              path="/login"
+              render={() => <LoginWidget config={oktaCongif} />}
+            />
+            <Route path="/login/callback" component={LoginCallback} />
+          </Switch>
+        </div>
 
-      <Footer />
+        <Footer />
+      </Security>
     </div>
   );
 };
---
+++ frontend/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaCongif = {
+  clientId: '0oa8qy4sbgyjikHQq5d7',
+  issuer: 'https://dev-73369614.okta.com/oauth2/default',
+  redirectUri: 'http://localhost:3000/login/callback',
+  scops: ['openid', 'profile', 'email'],
+  pkce: true,
+  disableHttpsChecks: true,
+};
