086c4a99d8948867f678aabf52d3678a508c2715
Micheal Limones <micheal@zestylimones.com>
2023-03-20 09:10:25 -0500

    added current loan count.

--- backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
+++ backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
@@ -57,4 +57,8 @@ public class BookService {
             return false;
         }
     }
+
+    public int currentLoanCount(String userEmail) {
+        return checkoutRepository.findBooksByUserEmail(userEmail).size();
+    }
 }
