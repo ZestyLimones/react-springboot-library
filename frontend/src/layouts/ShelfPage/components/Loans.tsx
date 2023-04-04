35a182e1b1215db1307d5feeea6d6a878c8a21bb
Micheal Limones <michlimones@gmail.com>
2023-04-04 12:02:27 -0500

    added renewal to frontend.

--- frontend/src/layouts/ShelfPage/components/Loans.tsx
+++ frontend/src/layouts/ShelfPage/components/Loans.tsx
@@ -71,6 +71,22 @@ export const Loans = () => {
     setCheckout(!checkout);
   }
 
+  async function renewLoan(bookId: number) {
+    const url = `http://localhost:8080/api/books/secure/renew/loan/?bookId=${bookId}`;
+    const requestOptions = {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    };
+    const returnResponse = await fetch(url, requestOptions);
+    if (!returnResponse.ok) {
+      throw new Error('Something went wrong!');
+    }
+    setCheckout(!checkout);
+  }
+
   return (
     <div>
       {/* Desktop */}
@@ -150,6 +166,7 @@ export const Loans = () => {
                   shelfCurrentLoan={shelfCurrentLoan}
                   mobile={false}
                   returnBook={returnBook}
+                  renewLoan={renewLoan}
                 />
               </div>
             ))}
@@ -239,6 +256,7 @@ export const Loans = () => {
                   shelfCurrentLoan={shelfCurrentLoan}
                   mobile={true}
                   returnBook={returnBook}
+                  renewLoan={renewLoan}
                 />
               </div>
             ))}
