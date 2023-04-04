a5e8ffc3dd5cb41abef25133118fd0e3724b3d79
Micheal Limones <michlimones@gmail.com>
2023-04-04 10:26:48 -0500

    created modal to allow users to manage book loan.

--- frontend/src/layouts/ShelfPage/components/Loans.tsx
+++ frontend/src/layouts/ShelfPage/components/Loans.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ShelfCurrentLoans from '../../../models/ShelfCurrentLoans';
 import { SpinnerLoading } from '../../Utils/SpinnerLoading';
+import { LoansModal } from './LoansModal';
 
 export const Loans = () => {
   const { authState } = useOktaAuth();
@@ -128,6 +129,10 @@ export const Loans = () => {
                   </div>
                 </div>
                 <hr />
+                <LoansModal
+                  shelfCurrentLoan={shelfCurrentLoan}
+                  mobile={false}
+                />
               </div>
             ))}
           </>
@@ -212,6 +217,7 @@ export const Loans = () => {
                 </div>
 
                 <hr />
+                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={true} />
               </div>
             ))}
           </>
