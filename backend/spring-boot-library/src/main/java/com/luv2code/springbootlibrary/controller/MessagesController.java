186ae241c10f80601b54376270b7541926ca03d9
Micheal Limones <michlimones@gmail.com>
2023-04-07 08:53:41 -0500

    updated backend to allow admin to response to messages.

--- backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
+++ backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
@@ -2,9 +2,11 @@ package com.luv2code.springbootlibrary.controller;
 
 
 import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.requestmodels.AdminQuestionRequest;
 import com.luv2code.springbootlibrary.service.MessagesService;
 import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.mediatype.alps.Ext;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:3000")
@@ -23,5 +25,15 @@ public class MessagesController {
     public void postMessage(@RequestHeader(value="Authorization") String token, @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization")String token, @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
     }
 }
